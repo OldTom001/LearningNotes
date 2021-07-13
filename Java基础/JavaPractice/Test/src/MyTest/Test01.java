7f222e8e103eca8204a0ed15e3fe5ef8dac0fe8b
SuelyChu <1021109865@qq.com>
2021-07-13 16:53:53 +0800

    加入Java基础练习

---
+++ Java基础/JavaPractice/Test/src/MyTest/Test01.java
@@ -0,0 +1,11 @@
+package MyTest;
+
+import java.util.Arrays;
+
+public class Test01 {
+    public static void main(String[] args) {
+        byte[] bys = {97, 98, 99};
+        String s = new String(bys);
+        System.out.println(s);
+    }
+}
