880d4bb2f1a06d78435df87af3adc801645d6de8
ZSL <1021109865@qq.com>
2022-03-10 12:22:40 +0800

    学习

---
+++ Java/Spring/Spring/2_ConnectionPool_Anno/src/main/java/web/UserController.java
@@ -0,0 +1,18 @@
+package web;
+
+import cofig.SpringCofiguration;
+import service.UserService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class UserController {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+//        ApplicationContext app = new AnnotationConfigApplicationContext(SpringCofiguration.class);
+        UserService userService = app.getBean(UserService.class);
+        userService.save();
+    }
+
+}
